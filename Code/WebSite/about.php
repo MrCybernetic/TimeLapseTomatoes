907112779ffd907878e4d839797c0f0ec4eeb616
MrCybernetic <cybernetic.msg@gmail.com>
2022-08-14 15:46:07 +0200

    Add 3D model views on about page

--- Code/WebSite/about.php
+++ Code/WebSite/about.php
@@ -6,32 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="img/icon.png">
-    <link rel="apple-touch-icon" href="img/icon.png"/>
+    <link rel="apple-touch-icon" href="img/icon.png" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <title>Cyber Cherry Tomatoes</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
-    <link rel="stylesheet" href="milligram.css?version=1.1">
+    <link rel="stylesheet" href="milligram.css?version=1.5">
 </head>
 
 <body>
     <?php require("header.php"); ?>
     <div class="flex-container-page">
-        <h1>About</h1>
-        <p>This site is for personnal purposes:</p>
-        <ul>
-            <li>Technical challenges: MQTT, NodeRed, Electronics, html, javascript, css, web server configuration, Git</li>
-            <li>Watch my plants</li>
-            <li>Share experiences with others</li>
-        </ul>
-        <h1>
-            <ion-icon name="logo-github"></ion-icon><a href="https://github.com/MrCybernetic">Github</a>
-        </h1>
-        <h1>
-            <ion-icon name="logo-twitter"></ion-icon><a href="https://twitter.com/Mr_Cybernetic">Twitter</a>
-        </h1>
+        <h1>The Stuff</h1>
+        <div class="flex-container-stuffs">
+            <div class="stuff">
+                <h2>Technos</h2>
+                <p>Attiny85, Chart.js, ESP-cam, ESP01, Git, Model-viewer, MQTT, NodeRed, KiCad, OnShape</p>
+            </div>
+            <div class="stuff">
+                <h2>Sensor</h2>
+                <model-viewer src="3d\Assembly_V4.gltf" shadow-intensity="1" ar ar-modes="webxr scene-viewer quick-look" camera-controls alt="A 3D model carousel">
+                    <div class="progress-bar hide" slot="progress-bar">
+                        <div class="update-bar"></div>
+                    </div>
+                </model-viewer>
+            </div>
+            <div class="stuff">
+                <h2>ESPCam</h2>
+                <model-viewer src="3d\Cyber_ESPcam.gltf" shadow-intensity="1" ar ar-modes="webxr scene-viewer quick-look" camera-controls alt="A 3D model carousel" orientation="0 -90deg 0">
+                    <div class="progress-bar hide" slot="progress-bar">
+                        <div class="update-bar"></div>
+                    </div>
+                </model-viewer>
+            </div>
+        </div>
+        <h1>The Cyborg</h1>
+        <p>
+            <a href="https://github.com/MrCybernetic">
+                <ion-icon name="logo-github"></ion-icon> Github
+            </a><a href="https://twitter.com/Mr_Cybernetic">
+                <ion-icon name="logo-twitter"></ion-icon> Twitter
+            </a>
+        </p>
     </div>
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
 </body>
 
 </html>
